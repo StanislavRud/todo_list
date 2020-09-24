2dda30767acdd19e0d603e6506c1ef3c104fa230
Stanislav <rudstanislav1990@gmail.com>
2020-09-24 17:00:24 +0300

    add useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TodoList from "./Todo/TodoList";
 
 function App() {
-    const todos = [
+    const [todo, setTodo] = useState([
         { id: 1, completed: false, title: "Купить хлеб" },
         { id: 2, completed: false, title: "Купить сидр" },
         { id: 3, completed: false, title: "Купить чипсы" },
         { id: 4, completed: false, title: "Купить суши"}
-    ];
+    ])
+
+
+    function toggleTodo(id) {
+        todos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        })
+    }
+
   return (
     <div className="wrapper">
         <h1>Hello</h1>
-        <TodoList todos={todos} />
+        <TodoList todos={todos} onToggle={toggleTodo}/>
     </div>
   );
 }
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -2,16 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import style from './TodoItem.module.css'
 
-const TodoItem = ({todo, index}) => {
+const TodoItem = ({todo, index, onChange}) => {
+    console.log('todo', todo)
     return (
         <li>
-            <p className={style.item__number}>{index + 1}</p>{todo.title}
+            <span>
+                <input type="checkbox"
+                       style={style.input}
+                       onChange={() => onChange(todo.id)}/>
+            <strong>{index + 1}</strong>
+                {todo.title}
+            </span>
+            <button className={style.rm}>&times;</button>
         </li>
+
     );
 };
 
 TodoItem.propTypes = {
-    todos: PropTypes.object.isRequired
-}
+    todo: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired,
+    onChange: PropTypes.func.isRequired
+};
 
 export default TodoItem;
